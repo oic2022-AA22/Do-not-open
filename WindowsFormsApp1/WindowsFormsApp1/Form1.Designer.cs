d40d4259a46dfa442aa8e94ae3b67553ec718af0
2C04 <C1062@oic.jp>
2022-10-07 14:01:07 +0900

    test

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace WindowsFormsApp1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(226, 161);
+            this.button1.Location = new System.Drawing.Point(226, 47);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(383, 126);
             this.button1.TabIndex = 0;
